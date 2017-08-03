4a76a7eb78675315ef846798bacbd7d795e3a6ff
jonathan <jonathanyi81@gmail.com>
2017-08-03 16:50:51 -0400

    finished scrape_top_list. now working on scraping movie page.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -5,11 +5,23 @@ class Scraper
   def self.scrape_top_list()
     html = File.read('./fixtures/IMDb_top_250.html')
     index = Nokogiri::HTML(html)
+
+    movie_list_array = index.css("tbody.lister-list tr").collect do |movie|
+      #movie title: movie.css("td.titleColumn a").text
+      #movie year: movie.css("td.titleColumn span.secondaryInfo").text
+      #movie url: movie.css("td.titleColumn a").attr("href").value
+      {:title => movie.css("td.titleColumn a").text,
+       :year => movie.css("td.titleColumn span.secondaryInfo").text,
+       :url => movie.css("td.titleColumn a").attr("href").value
+      }
+    end
+  end # scrape_top_list
+
+  def self.scrape_movie_page()
+    html = File.read('./fixtures/The Shawshank Redemption (1994) - IMDb.html')
+    index = Nokogiri::HTML(html)
     binding.pry
-  end
-
-  def self.scrap_movie_page(movie_url)
-
-  end
+    
+  end #scrape_movie_page
 
 end
