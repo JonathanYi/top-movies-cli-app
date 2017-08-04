4f88a459ff9f7f8666e443a836560114a71d33ae
jonathan <jonathanyi81@gmail.com>
2017-08-03 20:04:42 -0400

    added files genre and director. finished scraping for genre in the movie page

--- config/environment.rb
+++ config/environment.rb
@@ -1,6 +1,8 @@
 require 'bundler'
 Bundler.require
 
+require 'open-uri'
+require 'pry'
 
 require_all './lib'
 # require_relative '../lib/movie.rb'
--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,15 +1,14 @@
-require 'open-uri'
-require 'pry'
+# require 'open-uri'
+# require 'pry'
+require_relative '../config/environment.rb'
 
 class Scraper
+
   def self.scrape_top_list()
     html = File.read('./fixtures/IMDb_top_250.html')
     index = Nokogiri::HTML(html)
 
     movie_list_array = index.css("tbody.lister-list tr").collect do |movie|
-      #movie title: movie.css("td.titleColumn a").text
-      #movie year: movie.css("td.titleColumn span.secondaryInfo").text
-      #movie url: movie.css("td.titleColumn a").attr("href").value
       {:title => movie.css("td.titleColumn a").text,
        :year => movie.css("td.titleColumn span.secondaryInfo").text,
        :url => movie.css("td.titleColumn a").attr("href").value
@@ -20,8 +19,22 @@ class Scraper
   def self.scrape_movie_page()
     html = File.read('./fixtures/The Shawshank Redemption (1994) - IMDb.html')
     index = Nokogiri::HTML(html)
+
+    movie_details = {}
+    # movie class will create instances of genre and director and add movies to
+    # each.
+    movie_details[:genre] = index.css("div[itemprop='genre'] a").collect do |genre|
+      genre.text
+    end
+
     binding.pry
-    
+    #release
+    #storyline
+    #rating
+    #director
+    #genre
+    #run_time
+    movie_details
   end #scrape_movie_page
 
 end
